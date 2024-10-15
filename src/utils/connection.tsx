7e2bc73a269ce24df464024abf89b41564f98316
Bereket Alemayehu <86413773+Hilcbek@users.noreply.github.com>
2024-10-15 14:13:02 +0300

    main - 50

--- src/utils/connection.tsx
+++ src/utils/connection.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const baseURL = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'https://test-qvio.onrender.com/api',
   withCredentials: true,
 });
-//https://test-qvio.onrender.com
+//
